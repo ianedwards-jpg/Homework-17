4439c3fea3831560207d9ec11d797cf09e24defb
Ian Edwards <ian.edward@uconn.edu>
2020-07-07 18:19:15 -0400

    update

---
+++ Homework 17/Develop/models/resistance.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+console.log(user.email); // 'avenue@q.com'
+
+const workoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    default: "resistance"
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
+  },
+  duration: {
+    type: Number,
+    required: "Enter (in seconds) a duration"
+  },
+  weight: {
+    type: Number,
+    required: "Enter a weight"
+  },
+  reps: {
+    type: Number,
+    required: "Enter a number of reps"
+  },
+  sets: {
+    type: Number,
+    required: "Enter a number of sets"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- Homework 17/Develop/routes/router.js
+++ Homework 17/Develop/routes/router.js
@@ -3,8 +3,8 @@ const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
@@ -13,8 +13,8 @@ router.post("/api/workouts", ({ body }, res) => {
 
 router.post("/api/workouts/bulk", ({ body }, res) => {
   Workout.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
@@ -24,8 +24,8 @@ router.post("/api/workouts/bulk", ({ body }, res) => {
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
